af2994544bd0c31913500c9fbf85f70974c1c422
Ali Sharifi <sharifi.ali@hotmail.com>
2017-04-13 13:29:21 -0700

    ThreadStaticAttribute

---
+++ ThreadStatic/ThreadStatic/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadStatic
+{
+    class Program
+    {
+        [ThreadStatic]
+        public static int _field;
+
+        static void Main(string[] args)
+        {
+            new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    _field++;
+                    Console.WriteLine($"Thread A: {_field}");
+                }
+            }).Start();
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    _field++;
+                    Console.WriteLine($"Thread B: {_field}");
+                }
+            }).Start();
+            Console.ReadKey();
+        }
+    }
+}

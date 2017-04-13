783064cedbf5b18dea9395c46ce583ad325f87e1
Ali Sharifi <sharifi.ali@hotmail.com>
2017-04-13 12:45:35 -0700

    ParameterizedThreadStart

--- ThreadClass/ThreadClass/Program.cs
+++ ThreadClass/ThreadClass/Program.cs
@@ -11,23 +11,43 @@ namespace ThreadClass
     {
         static void Main(string[] args)
         {
-            Thread t = new Thread(new ThreadStart(ThreadMethod));
-            t.Start();
-            t.Priority = ThreadPriority.Highest;
+            Thread t1 = new Thread(new ThreadStart(ThreadMethod));
+            Thread t2 = new Thread(new ParameterizedThreadStart(ThreadMethod));
+
+            //t.Priority = ThreadPriority.Highest;
+            //t.IsBackground = true;
+            t1.Start();
+            t2.Start(5);
+
             for (int i = 0; i < 4; i++)
             {
                 Console.WriteLine("Main Thread : Do some work.");
+                if (i==1)
+                {
+                    t1.Abort();
+                    t2.Abort();
+                }
                 Thread.Sleep(0);
+
             }
-            t.Join();
+            t1.Join();
+            t2.Join();
         }
 
         private static void ThreadMethod()
         {
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine($"ThreadProc: {i}");
-                Thread.Sleep(0);
+                Console.WriteLine($"ThreadProc1: {i}");
+                Thread.Sleep(1000);
+            }
+        }
+        private static void ThreadMethod(object o)
+        {
+            for (int i = 0; i < (int)o; i++)
+            {
+                Console.WriteLine($"ThreadProc2: {i}");                
+                Thread.Sleep(1000);
             }
         }
     }

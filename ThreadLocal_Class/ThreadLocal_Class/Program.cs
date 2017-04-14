1a7df4025a168bcd063eb92a3393507bf1f06542
Ali Sharifi <sharifi.ali@hotmail.com>
2017-04-14 16:34:22 -0700

    ThreadLocal_Class

---
+++ ThreadLocal_Class/ThreadLocal_Class/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadLocal_Class
+{
+    class Program
+    {
+        public static ThreadLocal<int> _field =
+            new ThreadLocal<int>(() =>
+            {
+                return Thread.CurrentThread.ManagedThreadId;
+            });
+
+        static void Main(string[] args)
+        {
+            new Thread(() =>
+            {
+                for (int i = 0; i < _field.Value; i++)
+                {
+                    Console.WriteLine($"Thead A: {_field}");
+                }
+            }).Start();
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < _field.Value; i++)
+                {
+                    Console.WriteLine($"Thread B: {_field}");
+                }
+            }).Start();
+        }
+
+    }
+}
+

30726ad91472c41a42d323580db8d16e9b8833d5
Ali Sharifi <sharifi.ali@hotmail.com>
2017-04-15 10:50:29 -0700

    ThreadPool

---
+++ ThreadPool/ThreadPool/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Threading.ThreadPool.QueueUserWorkItem((s) =>
+                {
+                    Console.WriteLine("Working on a thread from ThreadPool");
+                });
+
+            Console.ReadKey();
+        }
+    }
+}

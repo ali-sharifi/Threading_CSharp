d29255c693bf6fcbc034cf996bd4fb514320a294
Ali Sharifi <sharifi.ali@hotmail.com>
2017-05-13 12:35:35 -0700

    TaskReturnsValues

---
+++ TaskReturnsValues/TaskReturnsValues/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskReturnsValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> t = Task.Run(() =>
+            {
+                return 50;
+            });
+            //t.Wait(); no need for this
+            Console.WriteLine(t.Result);
+        }
+    }
+}

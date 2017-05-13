606ef4d544dd96b6823a2e389023679c77765f13
Ali Sharifi <sharifi.ali@hotmail.com>
2017-05-13 12:30:17 -0700

    NewTask

---
+++ NewTask/NewTask/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t = Task.Run(() => {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine("*");
+                }
+            });
+
+            //Task t = new Task(() => {
+            //    for (int i = 0; i < 100; i++)
+            //    {
+            //        Console.WriteLine("*");
+            //    }
+            //});
+            //t.Start();
+            t.Wait();
+        }
+    }
+}
+

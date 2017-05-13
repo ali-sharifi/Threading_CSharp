487af9b6728f15d27efa24eb91a4493062e1d24b
Ali Sharifi <sharifi.ali@hotmail.com>
2017-05-13 12:50:01 -0700

    TaskWithContinuation

---
+++ TaskWithContinuation/TaskWithContinuation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskWithContinuation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> t = Task.Run(() =>
+            {
+                return 50;
+            });
+            Console.WriteLine(t.Result);
+
+            t.ContinueWith((i) =>
+                            {
+                                Console.WriteLine("Canceled");
+                            }, TaskContinuationOptions.OnlyOnCanceled);
+            t.ContinueWith((i) =>
+                            {
+                                Console.WriteLine("Faulted");
+                            }, TaskContinuationOptions.NotOnFaulted);
+
+            var completedTask = t.ContinueWith((i) =>
+                              {
+                                  Console.WriteLine("Completed");
+                              }, TaskContinuationOptions.OnlyOnRanToCompletion);
+            completedTask.Wait();
+        }
+    }
+}

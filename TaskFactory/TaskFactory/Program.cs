07d81a649f1d1f2ba4c47218a13b1e4743304fb1
Ali Sharifi <sharifi.ali@hotmail.com>
2017-05-13 19:10:35 -0700

    TaskFactory

---
+++ TaskFactory/TaskFactory/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<Int32[]> parent = Task.Run(() =>
+            {
+                var result = new Int32[3];
+                TaskFactory<int> tf = new TaskFactory<int>(TaskCreationOptions.AttachedToParent, TaskContinuationOptions.ExecuteSynchronously);
+                tf.StartNew(() => result[0] = 0);
+                tf.StartNew(() => result[1] = 1);
+                tf.StartNew(() => result[2] = 2);
+                return result;
+            });
+
+            var finalTask = parent.ContinueWith(
+                parentTask =>
+                {
+                    foreach (int i in parentTask.Result)
+                    {
+                        Console.WriteLine(i);
+                    }
+                });
+            finalTask.Wait();
+        }
+    }
+}

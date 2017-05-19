ad866bc699d9042f53fd6807ae2e956f0e23db04
Ali Sharifi <sharifi.ali@hotmail.com>
2017-05-18 22:21:48 -0700

    ConcurrentStack

---
+++ ConcurrentStack/ConcurrentStack/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrentStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcurrentStack<int> stack = new ConcurrentStack<int>();
+            stack.Push(42);
+
+            int result;
+            if (stack.TryPop(out result))
+            {
+                Console.WriteLine($"Popped: {result}");
+            }
+            stack.PushRange(new int[] { 1, 2, 3 });
+            int[] values = new int[3];
+            stack.TryPopRange(values);
+
+            foreach (var item in values)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}

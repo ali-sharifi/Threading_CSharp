a2548c0ec20d240f28b010eca72cd5894a118dc2
Ali Sharifi <sharifi.ali@hotmail.com>
2017-05-19 21:58:39 -0700

    CancellationToken

---
+++ CancellationToken/CancellationToken/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CancellationToken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cancelaltionTokenSource = new CancellationTokenSource();
+            var token = cancelaltionTokenSource.Token;
+
+            Task task = Task.Run(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    Console.WriteLine("*");
+                    Thread.Sleep(1000);
+                }
+                token.ThrowIfCancellationRequested();
+            }, token);
+
+            try
+            {
+                Console.WriteLine("Press enter to stop the task");
+                Console.ReadLine();
+                cancelaltionTokenSource.Cancel();
+                task.Wait();
+            }
+            //catch (OperationCanceledException ex)
+            //{
+            //    Console.WriteLine($"OperationCancellationException {ex.Message}");
+            //}
+            catch (AggregateException ex)
+            {
+                Console.WriteLine(ex.InnerException.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+            }
+            Console.WriteLine("Press enter to end the application");
+            Console.ReadLine();
+        }
+    }
+}
